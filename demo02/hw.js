bb438b43aa7f410cf15ced2e4a8ba25221d55a90
wrk1231 <wrk1231@gmail.com>
2019-05-18 19:19:04 -0400

    react

---
+++ demo02/hw.js
@@ -0,0 +1,11 @@
+// ReactDOM.render(){
+//     <span>hello world!</span>
+//     ,
+//     document.getElementById('element2')
+// };
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('examplee')
+  );
+  
